85bca680a448367f9ca35548dff08d55be157f88
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2024-06-06 21:58:27 +0100

    Created a component for managing tabs in the Restaurant view. This component enables users to switch between different sections like Menu and Reviews easily.

---
+++ app/(routes)/restaurant/_components/RestaurantTabs.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import MenuSection from './MenuSection';
+import ReviewSection from './ReviewSection';
+import AboutRest from './AboutRest';
+
+function RestaurantTabs({ restaurant }) {
+    return (
+        // Define tabs with default value and styling
+        <Tabs defaultValue="category" className="w-full mt-5 bg-white p-3 rounded-xl">
+            <TabsList>
+                {/* Trigger for the Menu tab */}
+                <TabsTrigger value="category">Menu</TabsTrigger>
+
+                {/* Trigger for the Reviews tab */}
+                <TabsTrigger value="review">Reviews</TabsTrigger>
+
+            </TabsList>
+            {/* Content for the About section (not currently used) */}
+            <TabsContent value="about"><AboutRest /></TabsContent>
+
+            {/* Content for the Menu section */}
+            <TabsContent value="category"><MenuSection restaurant={restaurant} /></TabsContent>
+
+            {/* Content for the Reviews section */}
+            <TabsContent value="review">
+                <ReviewSection restaurant={restaurant} />
+            </TabsContent>
+        </Tabs>
+    );
+}
+
+export default RestaurantTabs;
