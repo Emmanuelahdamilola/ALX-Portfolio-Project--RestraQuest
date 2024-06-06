51c1fcbb910a941b5908a0bde98dc9a1307d153b
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2024-06-06 21:11:55 +0100

    Added SignUpPage component to render the SignUp form provided by Clerk

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,7 @@
+import { SignUp } from "@clerk/nextjs";
+
+// This component renders the SignUp form provided by Clerk.
+// Users can sign up using this form.
+export default function SignUpPage() {
+  return <SignUp />;
+}
